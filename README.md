fdd38ebe0c41848af050f59b91920d2de7512a20
Grey Christoforo <grey@christoforo.net>
2016-01-13 14:30:19 +0000

    starting to form build.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # heatedXYZ
 Design for a heated XYZ stage
+This design takes inspiration from https://github.com/lautr3k/RepRap-iTopie
---
+++ python/build.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2
+FREECADPATH = '/usr/lib/freecad' # path to your FreeCAD.so or FreeCAD.dll file
+import sys
+sys.path.append(FREECADPATH)
+import FreeCAD
