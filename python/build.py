6b172f785a3b2708db2b1bbcf34d495441ad7a99
Grey Christoforo <grey@christoforo.net>
2016-01-26 23:02:41 +0000

    union now uses upgraded internal union with fuzzy logic

--- python/build.py
+++ python/build.py
@@ -16,20 +16,23 @@ def circle(radius):
     circFace = Part.Face(circWire)
     return circFace
 
-def union(thingA,thingB):
+# only tested/working with solid+solid and face+face
+def union(thingA,thingB,tol=1e-5):
     if (thingA.ShapeType == 'Face') and (thingB.ShapeType == 'Face'):
-        u = thingA.oldFuse(thingB).Faces[0]
+        u = thingA.multiFuse([thingB],tol).removeSplitter().Faces[0]
     elif (thingA.ShapeType == 'Solid') and (thingB.ShapeType == 'Solid'):
-        u = thingA.oldFuse(thingB).Solids[0]
+        u = thingA.multiFuse([thingB],tol).removeSplitter().Solids[0]
     else:
         u = []
     return u
 
+# TODO: this cut is leaving breaks in circles, try to upgrade it to fuzzy logic with tolerance
+# also remove splitter does nothing here
 def difference(thingA,thingB):
     if (thingA.ShapeType == 'Face') and (thingB.ShapeType == 'Face'):
-        d = thingA.cut(thingB).Faces[0]
+        d = thingA.cut(thingB).removeSplitter().Faces[0]
     elif (thingA.ShapeType == 'Solid') and (thingB.ShapeType == 'Solid'):
-        d = thingA.cut(thingB).Solids[0]
+        d = thingA.cut(thingB).removeSplitter().Solids[0]
     else:
         d = []
     return d
@@ -49,8 +52,9 @@ def extrude (face,direction):
     return face.extrude(FreeCAD.Vector(direction))
 
 def translate (obj,direction):
-    obj.translate(FreeCAD.Vector(direction))
-    return obj
+    tobj = obj.copy()
+    tobj.translate(FreeCAD.Vector(direction))
+    return tobj
 
 def section (solid,height="halfWay"):
     bb = solid.BoundBox
@@ -63,6 +67,7 @@ def section (solid,height="halfWay"):
     sectionShape = solid.section(slicePlane)
     return sectionShape
 
+# 2d first, union then extrude to 3d --> 2d section:
 myrect = rectangle(4,3)
 myrect = translate(myrect,(0,2,0))
 myrect2 = rectangle(1,10)
@@ -79,3 +84,19 @@ banjoSection = section(banjo3D)
 
 save2DXF(banjoSection,"banjo.dxf")
 solid2STEP(banjo3D, "banjo.step")
+
+# 2d first --> extrude to 3d then union --> 2d section:
+# this gives better results. boolean operations work better on 3d things
+myrect = extrude(rectangle(4,3), (0,0,thickness))
+myrect = translate(myrect,(0,2,0))
+myrect2 = extrude(rectangle(1,10), (0,0,thickness))
+circ=extrude(circle(4), (0,0,thickness))
+circ2=extrude(circle(2), (0,0,thickness))
+
+aShape = union(myrect,myrect2)
+banjoOuter2D = union(aShape,circ)
+banjo3D = difference(banjoOuter2D, circ2)
+banjoSection = section(banjo3D)
+
+save2DXF(banjoSection,"banjo2.dxf")
+solid2STEP(banjo3D, "banjo2.step")
